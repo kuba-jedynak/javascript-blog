6f89b32adfe1b4e61313a65a8a826a8ce8664692
Jakub Jedynak <kuba7486@gmail.com>
2019-11-17 15:12:04 +0100

    Add const targetArticle & add class active to the article

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@
     const titleClickHandler = function (event) {
         event.preventDefault();
         const clickedElement = this;
-        console.log('Link was clicked!');
-        console.log(event);
+        //console.log('Link was clicked!');
+        //console.log(event);
 
         /* [DONE] remove class 'active' from all article links  */
         const activeLinks = document.querySelectorAll('.titles a.active');
@@ -29,8 +29,11 @@
         console.log(articleSelector);
 
         /* find the correct article using the selector (value of 'href' attribute) */
+        const targetArticle = document.querySelector(articleSelector);
+        console.log(targetArticle);
 
         /* add class 'active' to the correct article */
+        targetArticle.classList.add('active');
     }
 
     const links = document.querySelectorAll('.titles a');
