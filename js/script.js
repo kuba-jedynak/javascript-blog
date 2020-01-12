1a7a11f191a1892793435242f511fb04c1a9bdbc
Jakub Jedynak <kuba7486@gmail.com>
2020-01-12 05:48:26 +0100

    Add tag list in sidebar

--- js/script.js
+++ js/script.js
@@ -28,7 +28,9 @@
     optTitleSelector = '.post-title',
     optTitleListSelector = '.titles',
     optArticleTagsSelector = '.post-tags .list',
-    optArticleAuthorSelector = '.post-author';
+    optArticleAuthorSelector = '.post-author',
+    optTagsListSelector = '.tags.list';
+    
 
   function generateTitleLinks(customSelector = '') {
     /* remove contents of titleList */
@@ -56,6 +58,8 @@
   generateTitleLinks();
 
   function generateTags() {
+    /*[NEW] create a new variable allTags with an empty array */
+    let allTags = [];
     /* find all articles */
     const articles = document.querySelectorAll(optArticleSelector);
     /* START LOOP: for every article: */
@@ -75,12 +79,21 @@
         const linkHTML = '<li><a href="#tag-' + tag + '">' + tag + ', </a></li> ';
         /* add generated code to html variable */
         html = html + linkHTML;
+        /* [NEW] check if this link is NOT already in allTags */
+        if(allTags.indexOf(linkHTML) == -1){
+          /* [NEW] add generated code to allTags array */
+          allTags.push(linkHTML);
+        }
       }
       /* END LOOP: for each tag */
       /* insert HTML of all the links into the tags wrapper */
       tagWrapper.innerHTML = html;
       /* END LOOP: for every article: */
     }
+    /* [NEW] find list of tags in right column */
+    const tagList = document.querySelector(' .tags');
+    /* [NEW] add html from allTags to tagList */
+    tagList.innerHTML = allTags.join(' ');
   }
   generateTags();
 
@@ -179,4 +192,6 @@
     }
   }
   addClickListenersToAuthors();
+
+  
 }
